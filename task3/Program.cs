5ac846aeb51bc230404940183350a567481d8a77
nyshashina <nyshashina@gmail.com>
2023-03-04 11:58:57 +0300

    task3 was done

---
+++ task3/Program.cs
@@ -0,0 +1,24 @@
+﻿// Показать натуральные числа от M до N, N и M заданы
+void ShowNums(int i, int number)
+{
+    Console.Write($"{i} ");
+    if (i >= number)
+    {
+        return;
+    }
+    i++;
+    ShowNums(i,number);
+}
+Console.WriteLine("Введите первое число");
+int numberM = int.Parse(Console.ReadLine() ?? "0");
+Console.WriteLine("Введите второе число");
+int numberN = int.Parse(Console.ReadLine() ?? "0");
+if(numberM <= numberN && numberM > 0 && numberN > 0)
+{
+    Console.WriteLine($"Натуральные числа от {numberM} до {numberN}: ");
+    ShowNums(numberM,numberN);
+}
+else
+{
+    Console.WriteLine("Числа должны быть натуральными. Первое число должно быть меньше второго.");
+}
