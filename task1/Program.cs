3461028db3026ac06dc173950f4659be581a2c8d
nyshashina <nyshashina@gmail.com>
2023-03-04 10:54:14 +0300

    task1 was done

---
+++ task1/Program.cs
@@ -0,0 +1,18 @@
+﻿// Показать натуральные числа от 1 до N, N задано
+/*int CountNums(int n)
+{
+    if (n > 1) return CountNums(n)+1;
+    else return 1;
+}
+Console.WriteLine(CountNums(10));*/
+void ShowNums(int n)
+{
+    for(int i = 1; i <= n; i++)
+    {
+        Console.Write($"{i} ");
+    }
+}
+Console.WriteLine("Введите число");
+int numberN = int.Parse(Console.ReadLine() ?? "0");
+Console.WriteLine($"Натуральные числа от 1 до {numberN}: ");
+ShowNums(numberN);
