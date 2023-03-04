13b47f5b9e35b391439abece4ca2e30212592e9d
nyshashina <nyshashina@gmail.com>
2023-03-04 11:46:51 +0300

    task1 was done with recursion

--- task1/Program.cs
+++ task1/Program.cs
@@ -1,18 +1,24 @@
 ﻿// Показать натуральные числа от 1 до N, N задано
-/*int CountNums(int n)
+void ShowNums(int i, int number)
 {
-    if (n > 1) return CountNums(n)+1;
-    else return 1;
+    Console.Write($"{i} ");
+    if (i >= number)
+    {
+        return;
+    }
+    i++;
+    ShowNums(i,number);
 }
-Console.WriteLine(CountNums(10));*/
-void ShowNums(int n)
+
+/*void ShowNums(int n)
 {
     for(int i = 1; i <= n; i++)
     {
         Console.Write($"{i} ");
     }
-}
+}*/
 Console.WriteLine("Введите число");
 int numberN = int.Parse(Console.ReadLine() ?? "0");
 Console.WriteLine($"Натуральные числа от 1 до {numberN}: ");
-ShowNums(numberN);
+int n = 1;
+ShowNums(n,numberN);
