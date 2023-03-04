d2d2cd2301d729a3f0ad737cb262039b68bf4e64
nyshashina <nyshashina@gmail.com>
2023-03-04 11:51:07 +0300

    task2 was done

---
+++ task2/Program.cs
@@ -0,0 +1,23 @@
+﻿// Показать натуральные числа от N до 1, N задано
+void ShowNums(int i, int number)
+{
+    Console.Write($"{number - i} ");
+    if (i >= number - 1)
+    {
+        return;
+    }
+    i++;
+    ShowNums(i,number);
+}
+/*void ShowNums(int n)
+{
+    for(int i = 0; i <= n; i++)
+    {
+        Console.Write($"{n-i} ");
+    }
+}*/
+Console.WriteLine("Введите число");
+int numberN = int.Parse(Console.ReadLine() ?? "0");
+Console.WriteLine($"Натуральные числа от {numberN} до 1: ");
+int n = 0;
+ShowNums(n,numberN);
