4ab4a1e02410e795193d3e9dfed532577b944715
nyshashina <nyshashina@gmail.com>
2023-03-04 13:33:10 +0300

    task6 was done

---
+++ task6/Program.cs
@@ -0,0 +1,18 @@
+﻿// Написать программу возведения числа А в целую стень B
+int ShowNums(int i, int number)
+{
+    if (number == 1)
+    {
+        return i;
+    }
+    number = number - 1;
+    return i*ShowNums(i,number);
+}
+Console.WriteLine("Введите число");
+int numberA = int.Parse(Console.ReadLine() ?? "0");
+Console.WriteLine("Введите степень");
+int numberB = int.Parse(Console.ReadLine() ?? "0");
+
+Console.Write($"Число {numberA}, возведенное в степень {numberB}, равно: ");
+Console.Write(ShowNums(numberA,numberB));
+
